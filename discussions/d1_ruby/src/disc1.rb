151fa87b800135fc5239d7a32f5c6a159ff06b02
soumikap <soumika.pendem@gmail.com>
2023-02-03 16:12:14 -0500

    ruby discussion

--- discussions/d1_ruby/src/disc1.rb
+++ discussions/d1_ruby/src/disc1.rb
@@ -2,6 +2,7 @@ def sum()
     a = [1,2,3,4,5]
     sum = 0
     # insert code block here
+    a.each { |s| sum += s}
 
     puts sum
 end
@@ -9,6 +10,7 @@ end
 def multiply()
     a = [1,2,3,4,5]
     # insert code block here
+    a.collect! { |s| s * 2}
 
     puts a.join(',')
 end
@@ -19,12 +21,21 @@ def population()
     countries["Italy"] = 60
 
     # insert code block here
+    countries.each { |k,v| 
+        puts "population of " + k + " is " + v + " million"
+        # puts "population of  #{k} is #{v} million"
+    }
 end
 
 def splitString(s)
     # insert code here
+    for word in s.split(',')
+		yield word
+	end
 end
 
 # call splitString here (param: "welcome,to,ruby,discussion") with code block
+splitstring("welcome,to,ruby,discussion") { |s| puts s}
 
 
+distance = Proc.new { |x1, y1, x1, y2| puts Math.sqrt((x2 - x1)**2 + (y2 - y1)**2) }
--- discussions/d1_ruby/src/rectangle.rb
+++ discussions/d1_ruby/src/rectangle.rb
@@ -1,13 +1,20 @@
 class Rectangle
     def initialize(length, width)
-    
+        @area = length * width
+        @@areas << @area
     end
 
     def getArea()
-    
+        return @area
     end
 
     def self.getNumRectangles(n)
-
+        count = 0
+        for a in @@areas
+            if a <= n
+                count += 1
+            end
+        end
+        return count
     end
 end
